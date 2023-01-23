53e63f0c945c4401b15a2fd778f73416806c37e1
Narek Balyan <narek_balyan@epam.com>
2023-01-23 18:14:12 +0400

    Added docker and docker-compose

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,10 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  @Get('a')
+  getA(): string {
+    return '1212389798231231233';
+  }
 
   @Get()
   getHello(): string {
